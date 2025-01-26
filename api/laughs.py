b3ba282b6f463fc2bdc11a9ef3eecd822fb1d88c
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2025-01-25 21:14:02 -0500

    The serverless function format

--- api/laughs.py
+++ api/laughs.py
@@ -1,14 +1,20 @@
+from http.server import BaseHTTPRequestHandler
 import json
 import random
 
-def handler(request):
-    jokes = [
-        "Why don't scientists trust atoms? Because they make up everything!",
-        "I'm reading a book on anti-gravity. It's impossible to put down.",
-        "What do you call a fake noodle? An impasta!",
-        "The other jokes here are chatgpt generated so if you see this you are lucky."
-    ]
-    return {
-        "statusCode": 200,
-        "body": json.dumps({"theJoke": random.choice(jokes)})
-    }
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        jokes = [
+            "Why don't scientists trust atoms? Because they make up everything!",
+            "I'm reading a book on anti-gravity. It's impossible to put down.",
+            "What do you call a fake noodle? An impasta!",
+            "The other jokes here are chatgpt generated so if you see this you are lucky."
+        ]
+        
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        
+        response = json.dumps({"theJoke": random.choice(jokes)})
+        self.wfile.write(response.encode())
+        return
